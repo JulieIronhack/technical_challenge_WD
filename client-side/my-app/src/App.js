f03e399a1918608902da084166ba9f9f7ba07c06
GJ <gjschoutsen@tutanota.com>
2022-03-24 22:49:43 +0100

    rendered phones

--- client-side/my-app/src/App.js
+++ client-side/my-app/src/App.js
@@ -1,26 +1,73 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import DetailsPage from "./pages/DetailsPage";
+import "./Loader.css";
 
 function App() {
-  const [phones, setPhones] = useState([]);
+  const [phones, setPhones] = useState();
+  const [showDetails, setShowDetails] = useState(false);
+  const [phoneDetails, setPhoneDetails] = useState();
 
   const fetchPhones = () => {
     axios
-      .get("localhost:3000/telephones")
+      .get("http://localhost:3000/telephones")
       .then((phones) => {
-        setPhones(phones.data);
+        //show spinner
+        setTimeout(() => setPhones(phones.data), 2000);
       })
-      .catch((e) => console.log("Error getting phones", e));
+      .catch((e) => console.log("Error getting phones from API  ", e));
   };
-
-  console.log(phones);
 
   useEffect(() => {
     fetchPhones();
   }, []);
 
-  return <div className="App"></div>;
+  const handleButton = (phone) => {
+    setPhoneDetails();
+    setPhoneDetails(phone);
+    setShowDetails(true);
+  };
+
+  return (
+    <div className="App">
+      <div className="loader-container">
+        {!phones && <div className="loader"></div>}
+      </div>
+      <div className="phone-details">
+        {showDetails && <DetailsPage phone={phoneDetails} />}
+      </div>
+      <div className="phone-list">
+        {phones &&
+          phones.map((phone) => {
+            return (
+              <div key={phone.id}>
+                <div className="phone">
+                  <div className="img-container">
+                    <img
+                      src={require(`./images/${phone.imageFileName}`)}
+                      alt="phone"
+                    />
+                  </div>
+                  <div className="phone-text">
+                    <div className="name">{phone.name}</div>
+                    <div className="man">{phone.manufacturer}</div>
+                    <div className="price"> € {phone.price},-</div>
+                    <button
+                      onClick={() => {
+                        handleButton(phone);
+                      }}
+                    >
+                      Show more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
