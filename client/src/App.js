e6ef7cf3cd3b47a63608243e9f5b2b4cefd2f12f
unknown <cesyrub@gmail.com>
2022-06-23 16:57:13 +0200

    change to a single page and some styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Phones from "./pages/Phones";
-import PhoneDetails from "./pages/PhoneDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Phones />}></Route>
-      <Route path="/:id" element={<PhoneDetails />}></Route>
     </Routes>
   );
 }
--- client/src/pages/Phones.jsx
+++ client/src/pages/Phones.jsx
@@ -1,9 +1,8 @@
-import logo from "../logo.svg";
 import "../App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import DotLoader from 'react-spinners/DotLoader'
-import { Link } from "react-router-dom";
+import PhoneDetailsComp from "../components/PhoneDetailsComp";
 
 function Phones() {
   const [phoneInfo, setPhoneInfo] = useState(null);
@@ -26,23 +25,15 @@ function Phones() {
       {phoneInfo === null && <div className="App flex flex-row flex-wrap justify-center items-center align-items-center h-full w-full"><DotLoader /></div>}
       {phoneInfo !== null && phoneInfo.map(elem => {
         return (
-          <Link to={`/${elem.id}`} key={elem._id}>
-            <div className="phone flex flex-row flex-wrap justify-center items-center align-items-center h-full w-full">
-              <div className='w-5/12 flex flex-row flex-wrap justify-end items-center align-items-center h-full w-full'>
-                <img src={elem.imageFileName} alt={elem.name} srcSet="" width={500} className="text-right rounded-3xl" />
-              </div>
-              <div className='w-5/12 bg-gray-900 p-7 rounded-2xl text-white'>
-                <p className="phoneTitle"><span className="font-bold">Phone name:</span> {elem.name}</p>
-                <p className="manufacturer"><span className="font-bold">Manufacturer:</span> {elem.manufacturer}</p>
-                <p className="description"><span className="font-bold">Description:</span> {elem.description}</p>
-                <p className="color"><span className="font-bold">Color:</span> {elem.color}</p>
-                <p className="price"><span className="font-bold">Price:</span> {elem.price}€</p>
-                <p className="screen"><span className="font-bold">Screen:</span> {elem.screen}</p>
-                <p className="processor"><span className="font-bold">Processor:</span> {elem.processor}</p>
-                <p className="ram"><span className="font-bold">Ram:</span> {elem.ram}GB</p>
-              </div>
+          <div className="phone flex flex-row flex-wrap justify-center items-center align-items-center h-full w-full">
+            <div className='w-5/12 flex flex-col flex-wrap justify-end items-center align-items-center h-full w-full' >
+              <img src={elem.imageFileName} alt={elem.name} srcSet="" width={500} className="text-right rounded-3xl" />
+              <p className="phoneTitle"><span className="font-bold"> {elem.name}</span></p>
             </div>
-          </Link>
+            <div className='w-5/12 bg-gray-900 p-7 rounded-2xl text-white text-sm lg:text-xl md:text-md mb-1 mt-1'>
+              <PhoneDetailsComp id={elem.id} />
+            </div>
+          </div>
         )
       })}
     </div>
