10dc8fd11bdf98ad4f41590ef10ec90f9e3d1a31
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-07-05 09:11:15 -0400

    create Phone model

---
+++ server/models/Phone.model.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require("mongoose");
+
+module.exports = model("Phone", new Schema({
+    name: { type: String, required: true, trim: true },
+    manufacturer: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    color: { type: String, required: true, trim: true },
+    price: { type: Number, required: true },
+    imageFile: { type: String, required: true },
+    screen: { type: String, required: true, trim: true },
+    processor: { type: String, required: true, trim: true },
+    ram: { type: Number, required: true }
+}));
