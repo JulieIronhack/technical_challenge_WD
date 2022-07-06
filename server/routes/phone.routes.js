58cf212e44768c1718f77dd31146a5ff0900a40f
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-07-05 21:01:19 -0400

    router to get one phone info

--- server/routes/phone.routes.js
+++ server/routes/phone.routes.js
@@ -4,11 +4,28 @@ const router = Router();
 
 const Phone = require("../models/Phone.model")
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
     try {
         const phones = await Phone.find();
         res.status(200).json(phones);
     } catch (error) {res.status(500).json({ message: "Error while trying to get all phones!", error: error.message })};
+});
+
+router.get("/:_id", async (req, res) => {
+    try {
+        const { _id } = req.params;
+        if (!(_id.length === 24) || !/^[a-z0-9]+$/.test(_id)) {
+            const error = new Error("Provided _id for the phone is invalid!");
+            error.status = 400;
+            throw error;
+        };
+
+        const phone = await Phone.findOne({ _id }, { __v: 0 });
+        res.status(200).json(phone);
+    } catch (error) {
+        if (!error.status) error.status = 500;
+        res.status(error.status).json({ message: "Error while trying one phone information!", error: error.message })
+    }
 });
 
 module.exports = router;
