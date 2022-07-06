93741c1305b2c79a259a15d0fc4b2f713885892d
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-07-06 10:57:53 -0400

    Iteration #2 | Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from "react";
+import phonesApi from "../utils/phonesApi";
+
 const Home = () => {
+    const [phones, setPhones] = useState([]);
+
+    useEffect(() => {setTimeout(async () => {
+        const phonesData = await phonesApi.getAllPhones();
+        setPhones(await phonesData);
+    }, 2500)}, []);
+
     return (
-        <h1>Home</h1>
+        <div>
+            {(phones.length === 0) && <h1>Loading</h1>}
+            {((phones.length > 0) && phones[0].error) && <h1>Error</h1>}
+            {((phones.length > 0) && !phones[0].error) && phones.map(phone => <h1 key={phone._id}>{phone.name}</h1>)}
+        </div>
     )
 };
 
--- client/src/utils/phonesApi.js
+++ client/src/utils/phonesApi.js
@@ -2,15 +2,17 @@ import axios from "axios";
 
 class PhonesApi {
     constructor() {
-        this.api = axios.create({ baseURL: `${process.env.REACT_APP_BEERS_URL}/phones` });
+        this.api = axios.create({ baseURL: `${process.env.REACT_APP_PHONES_URI}/phones` });
     };
 
-    getAllPhones = async setPhones => {
+    getAllPhones = async () => {
         try {
             const { data } = await this.api.get("/");
-            setPhones(await data);
+            return await data;
         } catch (error) {
             console.error("Error on getAllPhones", error);
         };
     };
 };
+
+export default new PhonesApi();
