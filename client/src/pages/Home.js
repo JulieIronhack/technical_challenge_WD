640a11a594ee577289922f0c754a975760158367
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-07-06 11:28:53 -0400

    PhoneCard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import phonesApi from "../utils/phonesApi";
 import Loading from "../components/Loading";
+import PhoneCard from "../components/PhoneCard";
 
 const Home = () => {
     const [phones, setPhones] = useState([]);
@@ -8,13 +9,13 @@ const Home = () => {
     useEffect(() => {setTimeout(async () => {
         const phonesData = await phonesApi.getAllPhones();
         setPhones(await phonesData);
-    }, 2500)}, []);
+    }, 1000)}, []);
 
     return (
         <div>
             {(phones.length === 0) && <Loading />}
             {((phones.length > 0) && phones[0].error) && <h1>Error</h1>}
-            {((phones.length > 0) && !phones[0].error) && phones.map(phone => <h1 key={phone._id}>{phone.name}</h1>)}
+            {((phones.length > 0) && !phones[0].error) && phones.map(phone => <PhoneCard key={phone._id} {...phone} />)}
         </div>
     )
 };
