4a4da1c063adbc81fae157b35b556e4117e42abc
Sebastian Marambio <hu.weee@gmail.com>
2022-07-14 16:49:51 +0200

    3rd

--- tech-challenge-server/routes/phone.routes.js
+++ tech-challenge-server/routes/phone.routes.js
@@ -1,11 +1,15 @@
+const data = require('../data/phones.json')
+
 const router = require("express").Router();
 
-router.get("/phones", (req, res, next) => {
-    res.json("Displaying all phones");
+router.get("/phones", async (req, res, next) => {
+    res.json(data);
   });
   
   router.get("/phones/:id", (req, res, next) => {
-    res.json("Displaying single phone");
+    const {id} = req.params
+    const singlePhone = data.filter(element => element.id === Number(id))
+    res.json(singlePhone);
   });
 
   module.exports = router;
