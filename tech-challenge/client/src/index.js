6fffb0f9c0bea33baf3d1ecdfcab5baca40b3447
EugeBr <eugebordenave@gmail.com>
2022-09-03 12:57:21 +0200

    update

--- tech-challenge/client/src/index.js
+++ tech-challenge/client/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- tech-challenge/client/src/pages/AllPhones.jsx
+++ tech-challenge/client/src/pages/AllPhones.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import LoadingComponent from "../components/Loading"
+import { Link } from 'react-router-dom';
 const API_URL = process.env.REACT_APP_API_URL
 
 const Phones = () => {
@@ -25,7 +26,9 @@ const Phones = () => {
             {
                 phones.map((phone) => {
                     return (
+                        <Link to={`/phones/${phone._id}`}>
                         <p>{phone.name}</p>
+                        </Link>
                     )
                 })
             }
