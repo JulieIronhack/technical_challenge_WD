068285696588d8e662df91a6f5271e2050b24b6d
EugeBr <eugebordenave@gmail.com>
2022-09-03 11:03:30 +0200

    all routes working in Postman

--- tech-challenge/server/routes/phone.routes.js
+++ tech-challenge/server/routes/phone.routes.js
@@ -4,11 +4,22 @@ const Phone = require("../models/Phone.model");
 
 router.get("/", async (req, res, next) => {
     try {
-          const phones = await Phone.find()
-          return res.status(200).json(phones)
-      } catch (error) {
-          next(error)
-      }
-  });
+        const phones = await Phone.find()
+        return res.status(200).json(phones)
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const phone = await Phone.findById(id);
+
+        return res.status(200).json({ phone })
+    } catch (error) {
+        next(error)
+    }
+});
 
 module.exports = router
