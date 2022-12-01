4a3db23a365b009bfbf6fde6205bd41996a71f2c
MoniIgnacio <moniignacio02@gmail.com>
2022-12-01 19:58:27 +0400

    routes GET done

--- CaveServer/routes/phones.routes.js
+++ CaveServer/routes/phones.routes.js
@@ -1,18 +1,26 @@
-const router = require ('express').Router();
+const router = require("express").Router();
+const Phone = require("../models/Phone.model");
 
 // GET '/phones' => Show all phones.
-router.get('/', (req,res,next) => {
-
-    res.status(200).json()
-
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await Phone.find();
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // GET '/phones/phonesId' => Show a phone details.
-router.get('/:phonesId', (req, res, next) => {
-    const {phonesId} = req.params;
+router.get("/:phonesId", async (req, res, next) => {
+  const { phonesId } = req.params;
 
-    res.status(200).json()
-
-})
+  try {
+    const responseId = await Phone.findById(phonesId);
+    res.status(200).json(responseId);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- CaveServer/seeds/seed-script.js
+++ CaveServer/seeds/seed-script.js
@@ -1,9 +1,12 @@
+// Buscar la data a agregar
 const phonesArr = require("./phones.seed.json");
 
+// Conectar a la Base de datos
 require("../db");
 
 const Phone = require("../models/Phone.model.js");
 
+// Agregar la data a la Base de datos
 Phone.insertMany(phonesArr)
   .then((response) => {
     console.log(response);
