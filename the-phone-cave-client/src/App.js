710385fe066c3bb81f8718e4c72349bdd47eebee
Thomas Augot <thomas.augot@hotmail.fr>
2023-02-16 16:33:07 +0100

    setting up react routes

--- the-phone-cave-client/src/App.js
+++ the-phone-cave-client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import PhoneList from "./pages/PhoneList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/phones" element={<PhoneList />} />
+      </Routes>
     </div>
   );
 }
