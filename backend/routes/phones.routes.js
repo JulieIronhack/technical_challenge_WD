5f8b7fc44cc9817e84a1fc4e25a5207000b2a146
zsorosz <oroszi.zsofi@gmail.com>
2023-03-22 16:42:09 +0100

    Add backend routes

--- backend/routes/phones.routes.js
+++ backend/routes/phones.routes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const phones = require("../data/phones.json");
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+  res.json(phones);
+});
+
+router.get("/:id", (req, res, next) => {
+  const phone = Object.keys(phones).find((item) => item === req.params.id);
+  res.json(phones[phone]);
 });
 
 module.exports = router;
