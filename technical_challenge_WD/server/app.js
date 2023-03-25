f0b9147b77e958bd5c2230d8e15eef7fadb39e37
Alfie Collins <alfiecollins44@gmail.com>
2023-03-25 15:30:08 +0000

    small changes

--- technical_challenge_WD/server/app.js
+++ technical_challenge_WD/server/app.js
@@ -16,12 +16,8 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
-
-
 const phoneRouter = require("./routes/phone.routes");
-app.use("/api",  isAuthenticated, phoneRouter);
+app.use("/api",  phoneRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
