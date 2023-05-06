5183835e79b15b548e5560745ddc449cd0dca554
artweise <art2wsxweise7ujm@gmail.com>
2023-05-06 13:56:35 +0200

    created page AllPhones, data is not displayed

--- phone-app-client/src/App.js
+++ phone-app-client/src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
+import AllPhones from "./pages/AllPhones/AllPhones";
+import PhoneDetails from "./pages/PhoneDetails/PhoneDetails";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/phones" element={<AllPhones />} />
+        <Route path="/phones/:id" element={<PhoneDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
