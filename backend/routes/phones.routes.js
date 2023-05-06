083a3e1f92170e65da0260b312fa435ef7546283
Thor Löfgren <lofgrenthor@gmail.com>
2023-05-06 10:38:52 +0200

    feature(phones routes): Finish backend routes
    - When sending a get request to the route "/phones", return a response
    that is an array of objects containing all the phone details.
    
    - When sending a get request to "/phones/:id" return a response that is
    an object containing the details of the phone matching the id.

--- backend/routes/phones.routes.js
+++ backend/routes/phones.routes.js
@@ -1,7 +1,17 @@
+const phonesData = require("../data/phones.json");
+
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+  res.json(phonesData);
+});
+
+router.get("/:id", (req, res, next) => {
+  const phoneId = req.params.id;
+
+  const phoneDetails = phonesData.find((phone) => phone.id === +phoneId);
+
+  res.json(phoneDetails);
 });
 
 module.exports = router;
