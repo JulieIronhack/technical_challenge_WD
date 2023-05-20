15fb0439366933621bad36779752788845838378
Javier <javiergonzalez74@hotmail.com>
2023-05-20 10:48:23 +0200

    api error handling

--- api/app.js
+++ api/app.js
@@ -1,8 +1,34 @@
 require('dotenv').config();
 
 const express = require ('express');
+const logger = require('morgan');
+const createError = require('http-errors');
 
 const app = express();
+
+app.use(logger('dev'))
+
+
+app.use((req, res, next) => {
+  next(createError(404, 'Route not found'))
+
+})
+
+app.use((error, req, res, next) => {
+  if (!error.status) {
+    error = createError(500, error)
+  }
+  console.error(error)
+
+  const data = {
+    message: error.message
+  }
+
+  res.status(error.status)
+  .json(data)
+
+
+})
 
 
 const port = process.env.PORT || 3001;
