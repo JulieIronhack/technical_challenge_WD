96cd35943f724b976976d23034044ba1d0b9976c
Victor Lopez <victorlpz2301@gmail.com>
2023-05-20 11:02:48 +0200

    Cors Config

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('./config/cors.config')
 
 
+app.use(cors)
 app.use(express.json());
 const api = require('./config/routes.config');
 app.use('/api/v1', api);
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.info(`Application is running at ${port}`)); 
