14acea2658b36661e74150f91a36a002726e0474
Ander Zulaika <anderzu85@gmail.com>
2023-05-20 10:52:07 +0200

    API created

---
+++ api/app.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.info(`Application is running at port ${port}`));
