41b640ec3ca6032361f1ce3b6de86ecd1930c839
Erica Sanchez <ericazach@gmail.com>
2023-05-20 10:37:28 +0200

    start api settings

---
+++ api/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+const createError = require("http-errors");
+
+app.use(logger("dev"));
+
+app.use((req, res, next) => next(createError(404, "Route not found")));
+
+app.use((error, req, res, next) => {
+  if (!error.code) {
+    error = createError(500, error);
+  }
+  const data = {
+    message: error.message,
+  };
+  res.status(error.status).json(data);
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.info(`App is running in port ${port}`));
