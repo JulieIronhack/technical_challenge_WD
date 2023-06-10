92f577da8cb3e94d410ecf3937b7227b022b2e80
Nana Chuto <mail@nanachuto.com>
2023-06-10 17:16:40 +0700

    Put phones in dropdown menu

--- technical-challenge-WD/src/components/PhoneList.jsx
+++ technical-challenge-WD/src/components/PhoneList.jsx
@@ -1,8 +1,20 @@
+import * as React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 const PhoneDetails = () => {
   const [phones, setPhones] = useState([]);
+  const [phone, setPhone] = React.useState("");
+
+  const handleChange = (event) => {
+    setPhone(event.target.value);
+  };
 
   // Fetch all phone data
   useEffect(() => {
@@ -18,12 +30,26 @@ const PhoneDetails = () => {
 
   return (
     <div>
-      <h2>Phone List</h2>
-      <ul>
-        {phones.map((phone) => (
-          <li key={phone.id}>{phone.name}</li>
-        ))}
-      </ul>
+      <h2>The Phone Cave</h2>
+
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Select Phone</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={phone}
+            label="Phone"
+            onChange={handleChange}
+          >
+            {phones.map((phone) => (
+              <MenuItem key={phone.id} value={phone.id}>
+                {phone.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 };
