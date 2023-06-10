a400c1f0a03064feecbef43b0f6e2adee4bb1b17
Nana Chuto <mail@nanachuto.com>
2023-06-10 17:25:47 +0700

    Add simulated spinner

--- technical-challenge-WD/src/components/PhoneList.jsx
+++ technical-challenge-WD/src/components/PhoneList.jsx
@@ -7,49 +7,87 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const PhoneDetails = () => {
   const [phones, setPhones] = useState([]);
   const [phone, setPhone] = React.useState("");
+  const [selectedPhone, setSelectedPhone] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleChange = (event) => {
-    setPhone(event.target.value);
+    const selectedPhoneId = event.target.value;
+    setPhone(selectedPhoneId);
+
+    const selectedPhone = phones.find((phone) => phone.id === selectedPhoneId);
+    setSelectedPhone(selectedPhone);
   };
 
   // Fetch all phone data
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/phones")
-      .then((response) => {
-        setPhones(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching phone data:", error);
-      });
+    setIsLoading(true);
+
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      axios
+        .get("http://localhost:5000/phones")
+        .then((response) => {
+          setPhones(response.data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching phone data:", error);
+          setIsLoading(false);
+        });
+    }, 1500);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <div>
-      <h2>The Phone Cave</h2>
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "1rem",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <>
+          <h2>The Phone Cave</h2>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Select</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={phone}
+                label="Phone"
+                onChange={handleChange}
+              >
+                {phones.map((phone) => (
+                  <MenuItem key={phone.id} value={phone.id}>
+                    {phone.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        </>
+      )}
 
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Select Phone</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={phone}
-            label="Phone"
-            onChange={handleChange}
-          >
-            {phones.map((phone) => (
-              <MenuItem key={phone.id} value={phone.id}>
-                {phone.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>
+      {!isLoading && selectedPhone && (
+        <div>
+          <h3>Phone Details</h3>
+          <p>Name: {selectedPhone.name}</p>
+          <p>Manufacturer: {selectedPhone.manufacturer}</p>
+          <p>Description: {selectedPhone.description}</p>
+        </div>
+      )}
     </div>
   );
 };
